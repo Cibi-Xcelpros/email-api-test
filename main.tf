41cc4443e6fe06e9033436e5d4af1a31f20843eb
Cibi-Xcelpros <165359840+Cibi-Xcelpros@users.noreply.github.com>
2024-05-23 17:30:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "send_email" {
   provisioner "local-exec" {
     command = <<EOT
-        curl -X POST "http://${var.env["EMAIL_API_URL"]}:2222/sendemail" \
+        curl -X POST "http://${var.email_service_ip}:2222/sendemail" \
              -d '{"api_key": "your_api_key", "tenant_name": "${var.tenant_name}", "subdomain": "${var.sub_domain_name}", "git_branch": "${var.git_branch}", "to_email_id": "${var.email_id_notification}"}' \
              -H "Content-Type: application/json"
       EOT
