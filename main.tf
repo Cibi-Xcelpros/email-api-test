380b7208651c0ec907ec309fca545f06c363381d
Cibi-Xcelpros <165359840+Cibi-Xcelpros@users.noreply.github.com>
2024-05-23 17:24:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-provider "null" {}
-
-variable "env" {
-  default = {}
-}
-
 resource "null_resource" "send_email" {
   provisioner "local-exec" {
-    environment = {
-      EMAIL_API_URL = lookup(var.env, "EMAIL_API_URL", "127.0.0.1")
-    }
     command = <<EOT
         curl -X POST "http://${EMAIL_API_URL}:2222/sendemail?tenant_name=${var.tenant_name}&subdomin=${var.sub_domain_name}&git_branch=${var.git_branch}&to_email_id=${var.email_id_notification}"
       EOT
