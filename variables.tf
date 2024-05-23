bf6bf05428a9fae3664d1b9df2508df024cc4c46
Cibi-Xcelpros <165359840+Cibi-Xcelpros@users.noreply.github.com>
2024-05-23 17:23:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,20 @@
-variable "EMAIL_API_URL" {
-  description = "Email API URL"
+variable "email_id_notification" {
+  description = "Enter the desired Email address to recive notification about new tenant launch"
+  type = string
+}
+variable "email_service_ip" {
+  description = "Enter the ip address of the email service"
+  type = string
+}
+variable "sub_domain_name" {
+  description = "Enter the your desired subdomain add, Will produce <subdomain>.weberp.xcelpros.com"
+  type = string
+}
+variable "git_branch" {
+  description = "Deployment branch"
+  type = string
+}
+variable "tenant_name" {
+  description = "Name of the Tenant"
   type = string
 }
