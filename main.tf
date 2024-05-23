398b40225c320f08f74c3b76206ebb5c9192eb89
Cibi-Xcelpros <165359840+Cibi-Xcelpros@users.noreply.github.com>
2024-05-23 15:26:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,25 @@
 #   }
 # }
 
+# resource "null_resource" "send_email" {
+#   provisioner "local-exec" {
+#     command = "curl '${var.EMAIL_API_URL}:2222/ping'"
+#   }
+# }
+
+provider "null" {}
+
+variable "env" {
+  default = {}
+}
+
 resource "null_resource" "send_email" {
   provisioner "local-exec" {
-    command = "curl '${var.EMAIL_API_URL}:2222/ping'"
+    environment = {
+      EMAIL_API_URL = lookup(var.env, "EMAIL_API_URL", "127.0.0.1")
+    }
+    command = <<EOT
+        curl -X POST "http://${EMAIL_API_URL}:2222/sendemail?tenant_name=Tenant04&subdomin=domain&git_branch=git-branch&to_email_id=cibi.chakravarthi@xcelpros.com"
+      EOT
   }
 }
