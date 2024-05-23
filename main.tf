55560b426bb896e6e478b620b788bd5a54af3168
Cibi-Xcelpros <165359840+Cibi-Xcelpros@users.noreply.github.com>
2024-05-23 14:37:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "send_email" {
   provisioner "local-exec" {
-    command = "curl '${var.EMAIL_API_URL}:3000/sendemail?tenant_name=Tenant04&subdomin=domain&git_branch=git-branch&to_email_id=cibi.chakravarthi@xcelpros.com'"
+    command = "curl '${var.EMAIL_API_URL}:2222/sendemail?tenant_name=Tenant04&subdomin=domain&git_branch=git-branch&to_email_id=cibi.chakravarthi@xcelpros.com'"
   }
 }
